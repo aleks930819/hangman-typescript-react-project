c21150920a70b03a9106997a951a564e0bf62e71
Aleksandar Atanasov <aleks930819@gmail.com>
2023-03-10 14:04:41 +0200

    added keyboard

--- src/components/Keyborad.tsx
+++ src/components/Keyborad.tsx
@@ -1,7 +1,46 @@
-type Props = {};
+import styles from './Keyboard.module.css';
 
-const Keyborad = (props: Props) => {
-  return <div>Keyborad</div>;
+const KEYS = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+const Keyborad = () => {
+  return (
+    <div className={styles.container}>
+      {KEYS.map((key) => {
+        return (
+          <button className={styles.key} key={key}>
+            {key}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Keyborad;

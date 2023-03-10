b479e03c1a64c2a3046252cc3e5b75a9bce9c01b
Aleksandar Atanasov <aleks930819@gmail.com>
2023-03-10 13:59:57 +0200

    added words view

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,8 +1,28 @@
-
+import styles from './HangmanWord.module.css';
 type Props = {};
 
 const HangmanWord = (props: Props) => {
-  return <div>HangmanWord</div>;
+  const word = 'test';
+  const guessedLetters = ['', 'e'];
+  return (
+    <div className={styles.container}>
+      {word.split('').map((letter, index) => {
+        return (
+          <span className={styles.letter} key={index}>
+            <span
+              style={{
+                visibility: guessedLetters.includes(letter)
+                  ? 'visible'
+                  : 'hidden',
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
 };
 
 export default HangmanWord;
